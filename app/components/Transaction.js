cceff9fb9475f4d116f3a963ce7381bd4c2c3b7e
mdfr11 <prikhodko.d1997@yandex.ru>
2019-07-05 02:14:51 +0300

    rendering transactions

--- app/components/Transaction.js
+++ app/components/Transaction.js
@@ -1,33 +1,48 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, FlatList, Dimensions } from "react-native";
 
-class Transaction extends Component { 
-    render() {
-        return(
-            <View style={container}>
-                <Text>Start</Text>
-                <Text>BTC/USD</Text>
-            </View>
-        )
-    }
+class Transaction extends Component {
+  render() {
+    const { transactions } = this.props;
+    return (
+      <FlatList
+        data={transactions}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <View style={container}>
+            <Text style={text}>{item.id}</Text>
+            <Text style={text}>{item.date}</Text>
+            <Text style={text}>{item.coin}</Text>
+            <Text style={text}>{item.amount}</Text>
+            <Text style={text}>{item.price}</Text>
+          </View>
+        )}
+      />
+    );
+  }
 }
 
+const screenWidth = Math.round(Dimensions.get("window").width);
+
 const styles = StyleSheet.create({
-    header: {
-        fontSize: 30,
-        fontWeight: "bold",
-        padding: 10
-    },
-    container: {
-        padding: 30,
-        borderColor: '#99AAB5',
-        borderWidth: 1,
-        backgroundColor: '#1E2223',
-    },
-})
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    padding: 10
+  },
+  container: {
+    padding: 15,
+    borderRadius: 5,
+    backgroundColor: "#1E2223",
+    width: screenWidth * 0.9,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  text: {
+    color: '#787878'
+  }
+});
 
-const {
-    header, container
-} = styles;
+const { header, container, text } = styles;
 
-export default Transaction
+export default Transaction;
--- app/containers/TransactionsCont.js
+++ app/containers/TransactionsCont.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
+import { Constants, SQLite } from 'expo';
 import { connect } from "react-redux";
 import { FetchCoinData } from "../actions/";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import { Button } from "react-native-elements";
+import Transaction from "../components/Transaction";
+
+const db = SQLite.openDatabase("db.db");
 
 class TransactionsCont extends Component {
+  state = {
+    transactions: null
+  };
   componentDidMount() {
     this.props.FetchCoinData();
+    this.update();
   }
   render() {
     const { crypto, navigation } = this.props;
-    console.log("aaaaaaa " + JSON.stringify(crypto));
+    const { transactions } = this.state;
+    
+    /* if (transactions === null || transactions.length === 0) {
+      return null;
+    } */
+
     return (
       <View style={container}>
+        <Transaction transactions={transactions}/>
         <Button
           title="Add transaction"
           onPress={() => navigation.navigate("AddTransaction")}
@@ -26,16 +40,27 @@ class TransactionsCont extends Component {
       </View>
     );
   }
+  update() {
+    db.transaction(tx => {
+      tx.executeSql("select * from transactions", [], (_, { rows }) => this.setState({ transactions: rows._array }), null, this.update
+      );
+    });
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    marginTop: 35,
+  },
+  transaction: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   }
 });
 
-const { container } = styles;
+const { container, transaction } = styles;
 
 const mapStateToProps = state => ({
   crypto: state.crypto
