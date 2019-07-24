665714f1dee2bd33f55c47ba00ab24e3c1fe0e54
mdfr11 <prikhodko.d1997@yandex.ru>
2019-07-24 03:47:48 +0300

    refreshing, delete from db

--- app/components/Transaction.js
+++ app/components/Transaction.js
@@ -2,31 +2,54 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, FlatList, Dimensions } from "react-native";
 import Chart from "./Chart";
 import { Constants } from "expo";
-import { LineChart, XAxis, Grid } from 'react-native-svg-charts'
+import AntDesign from "react-native-vector-icons/AntDesign";
 import { SQLite } from "expo";
+import { Button } from "react-native-elements";
 
 const db = SQLite.openDatabase("db.db");
 
 class Transaction extends Component {
   render() {
-    const qqq = 3591
-    const www = 3345
-    const data = [123,234,32,4,234,23,4,23342,23,4,23,4,2]
-    const {item, update} = this.props
+    const { item, update, id } = this.props;
     return (
-        <View>
-          <View style={container}>
-            <Text onPress={() => update}>sadasd</Text>
-            <Text style={text}>TransactionID {item.id}</Text>
-            <Text style={text}>Date {item.date}</Text>
-            <Text style={text}>Pair {item.pair}</Text>
-            <Text style={text}>Amount {item.amount}</Text>
-            <Text style={text}>Buy price {item.price}</Text>
-            <Text style={text}>Cost {item.lprice}</Text>
-            <Text style={text}>Worth {item.lprice * item.amount}</Text>
-            <Chart buyingPrice={item.price} percentq={( (item.amount*item.price) < (item.lprice * item.amount) ? (item.lprice * item.amount)/(item.amount*item.price)*100-100 : (item.lprice * item.amount)/(item.amount*item.price)*100-100)}/>
-          </View>
+      <View>
+        <View style={container}>
+          <Button
+            onPress={() =>
+              db.transaction(
+                tx => {
+                  tx.executeSql(`delete from transactions where id = ?;`, [
+                    item.id
+                  ]);
+                },
+                null,
+                update
+              )
+            }
+            icon={<AntDesign name="close" size={30} color="#D3BD83" />}
+            type="clear"
+          />
+          <Text style={text}>TransactionID {item.id}</Text>
+          <Text style={text}>Date {item.date}</Text>
+          <Text style={text}>Pair {item.pair}</Text>
+          <Text style={text}>Amount {item.amount}</Text>
+          <Text style={text}>Buy price {item.price}</Text>
+          <Text style={text}>Cost {item.lprice}</Text>
+          <Text style={text}>Worth {item.lprice * item.amount}</Text>
+          <Chart
+            buyingPrice={item.price}
+            percentq={
+              item.amount * item.price < item.lprice * item.amount
+                ? ((item.lprice * item.amount) / (item.amount * item.price)) *
+                    100 -
+                  100
+                : ((item.lprice * item.amount) / (item.amount * item.price)) *
+                    100 -
+                  100
+            }
+          />
         </View>
+      </View>
     );
   }
 }
@@ -55,7 +78,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingTop: Constants.statusBarHeight
-  },
+  }
 });
 
 const { header, container, text, containerChart } = styles;
--- app/containers/TransactionsCont.js
+++ app/containers/TransactionsCont.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { SQLite } from "expo";
 import { connect } from "react-redux";
 import { FetchCoinData } from "../actions/";
-import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  RefreshControl,
+  View,
+  FlatList,
+  ScrollView
+} from "react-native";
 import { Button } from "react-native-elements";
 import Transaction from "../components/Transaction";
-import _ from 'lodash';
+import _ from "lodash";
 
 const db = SQLite.openDatabase("db.db");
 
@@ -13,7 +19,8 @@ class TransactionsCont extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactions: null
+      transactions: null,
+      refreshing: false
     };
   }
   componentDidMount() {
@@ -22,59 +29,78 @@ class TransactionsCont extends Component {
   render() {
     const { crypto, navigation } = this.props;
     const { transactions } = this.state;
-    const cryptoTransform = crypto.map( ({lprice, curr1, curr2}) => ({lprice, pair: curr1 + '/' + curr2}) )
+    const cryptoTransform = crypto.map(({ lprice, curr1, curr2 }) => ({
+      lprice,
+      pair: curr1 + "/" + curr2
+    }));
     /*const mergeArrays = _.values(_.merge(
       _.keyBy(transactions, 'pair'),
       _.keyBy(cryptoTransform, 'pair')
     ))*/
     function gggg(transactions, cryptoTransform) {
-      var lll = []
-      if(transactions && cryptoTransform) {
-        transactions.map( (t) => {
-          cryptoTransform.map( (c) => {
-            if(t.pair === c.pair) {
-              lll.push(_.merge(t,c))
+      var lll = [];
+      if (transactions && cryptoTransform) {
+        transactions.map(t => {
+          cryptoTransform.map(c => {
+            if (t.pair === c.pair) {
+              lll.push(_.merge(t, c));
             }
-          } )
-        } )
-      } 
-      return _.uniq(lll)
+          });
+        });
+      }
+      return _.uniq(lll);
     }
     return (
-      <ScrollView>
-      <View style={container}>
-        <FlatList
-        data={gggg(transactions, cryptoTransform)}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <Transaction item={item} update={this.update}/>
-        )} />
-        <Button
-          title="Add transaction"
-          onPress={() => navigation.navigate("AddTransaction")}
-          buttonStyle={{
-            width: 150,
-            backgroundColor: "#D3BD83"
-          }}
-        />
-      </View>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }
+      >
+        <View style={container}>
+          <FlatList
+            data={gggg(transactions, cryptoTransform)}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <Transaction item={item} update={this.update} id={item.id} />
+            )}
+          />
+          <Button
+            title="Add transaction"
+            onPress={() => navigation.navigate("AddTransaction")}
+            buttonStyle={{
+              width: 150,
+              backgroundColor: "#D3BD83"
+            }}
+          />
+        </View>
       </ScrollView>
     );
   }
-  update() {
+  update = async () => {
     db.transaction(tx => {
       tx.executeSql(
         "select * from transactions",
         [],
         (_, { rows }) => {
-          this.setState({ transactions: rows._array })
-          Object.values(rows._array).map( ({pair}) => this.props.FetchCoinData(pair) );
+          this.setState({ transactions: rows._array });
+          Object.values(rows._array).map(({ pair }) =>
+            this.props.FetchCoinData(pair)
+          );
         },
         null,
         this.update
       );
-    })
-  }
+    });
+  };
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    this.update().then(() => {
+      this.setState({ refreshing: false });
+    });
+  };
 }
 
 const styles = StyleSheet.create({
